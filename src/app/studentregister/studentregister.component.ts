5a509441777701bc4d402e74264bcc0f973e2542
FelixGtz <felix1699@gmail.com>
2020-10-17 20:54:46 -0700

    Cambios en zoom

--- src/app/studentregister/studentregister.component.ts
+++ src/app/studentregister/studentregister.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Curso } from 'src/models/curso';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { dataBinding } from '@syncfusion/ej2-angular-schedule';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { MsgService } from 'src/services/msg.service';
 @Component({
   selector: 'app-studentregister',
   templateUrl: './studentregister.component.html',
@@ -20,18 +21,18 @@ export class StudentregisterComponent implements OnInit {
   curso: Curso;
   breakpoint;
 
- 
-  formSesiones:FormGroup
-  constructor(private db: AngularFirestore, private activeRoute: ActivatedRoute, public dialog: MatDialog, private fb:FormBuilder, private http:HttpClient) { }
+
+  formSesiones: FormGroup
+  constructor(private db: AngularFirestore, private activeRoute: ActivatedRoute, public dialog: MatDialog, private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getCurso();
-    
-    this.formSesiones=this.fb.group({
-      sesiones:['0']
+
+    this.formSesiones = this.fb.group({
+      sesiones: ['0']
     })
 
-    this.breakpoint = (window.innerWidth<=900)?1:2;
+    this.breakpoint = (window.innerWidth <= 900) ? 1 : 2;
   }
   getCurso() {
     var id = this.activeRoute.snapshot.params.idCurso;
@@ -48,22 +49,23 @@ export class StudentregisterComponent implements OnInit {
     })
   }
 
- 
- 
+
+
   openDialog(): void {
 
     const dialogRef = this.dialog.open(dialogStudent, {
       width: '1000px',
-      height:'500px',
-      data: { curso: this.curso,sesiones:this.formSesiones.value.sesiones}
+      height: '500px',
+      data: { curso: this.curso, sesiones: this.formSesiones.value.sesiones }
     });
   }
 
   onResize(event) {
-    this.breakpoint = (event.target.innerWidth<=900) ? 1:2;
+    this.breakpoint = (event.target.innerWidth <= 900) ? 1 : 2;
+  }
+  pagar() {
   }
 
-  
 }
 @Component({
   selector: 'dialogStudent',
@@ -71,21 +73,21 @@ export class StudentregisterComponent implements OnInit {
   styleUrls: ['./dialogStudent.css'],
 })
 export class dialogStudent implements OnInit {
-  clunes:Array<any>=new Array();
-  cmartes:Array<any>=new Array();
-  cmiercoles:Array<any>=new Array();
-  cjueves:Array<any>=new Array();
-  cviernes:Array<any>=new Array();
-  csabado:Array<any>=new Array();
-  cdomingo:Array<any>=new Array();
+  clunes: Array<any> = new Array();
+  cmartes: Array<any> = new Array();
+  cmiercoles: Array<any> = new Array();
+  cjueves: Array<any> = new Array();
+  cviernes: Array<any> = new Array();
+  csabado: Array<any> = new Array();
+  cdomingo: Array<any> = new Array();
 
-  asesorias:Array<any>=new Array();
+  asesorias: Array<any> = new Array();
   constructor(
     public dialogRef: MatDialogRef<dialogStudent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private db: AngularFirestore,private auth: AngularFireAuth) { }
+    @Inject(MAT_DIALOG_DATA) public data: any, private db: AngularFirestore, private auth: AngularFireAuth, private msg: MsgService, private router: Router) { }
   ngOnInit(): void {
     this.getHorario()
-    
+
   }
   onNoClick(): void {
     this.dialogRef.close();
@@ -94,70 +96,71 @@ export class dialogStudent implements OnInit {
     this.db.collection('horario').get().subscribe((res) => {
       res.docs.forEach((item) => {
         let h = item.data();
- 
+
         h.id = item.id
         if (h.id == this.data.curso.user.uid) {
 
-         this.clunes=h.lunes;
-         this.cmartes=h.martes;
-         this.cmiercoles=h.miercoles;
-         this.cjueves=h.jueves;
-         this.cviernes=h.viernes;
-         this.csabado=h.sabado;
-         this.cdomingo=h.domingo;
-      
+          this.clunes = h.lunes;
+          this.cmartes = h.martes;
+          this.cmiercoles = h.miercoles;
+          this.cjueves = h.jueves;
+          this.cviernes = h.viernes;
+          this.csabado = h.sabado;
+          this.cdomingo = h.domingo;
+
         }
       })
     })
   }
 
-  prueba(hour,dia){
-    var fecha=new Date();
-    var d=new Date().getDay();
-    if(this.data.sesiones>0){
+  prueba(hour, dia) {
+    var fecha = new Date();
+    var d = new Date().getDay();
+    if (this.data.sesiones > 0) {
 
-      if(d<dia){
-        fecha.setDate(fecha.getDate()+(dia-d))
-        
+      if (d < dia) {
+        fecha.setDate(fecha.getDate() + (dia - d))
+
         this.asesorias.push({
-          fecha:fecha,
-          hora:hour
+          fecha: fecha,
+          hora: hour
         })
 
       }
       this.data.sesiones--;
-   
+
     }
-   
-    
+
+
   }
 
-  save(){
-if (this.data.sesiones==0){
-  var user=this.auth.auth.currentUser;
-  console.log(this.asesorias)
-  //para identificar la asesoria se le suma el id del usuario con la id  del curso
-   this.db.collection('asesorias').doc(user.uid+this.data.curso.id).set({
+  save() {
+    if (this.data.sesiones == 0) {
+      var user = this.auth.auth.currentUser;
+      console.log(this.asesorias)
+      //para identificar la asesoria se le suma el id del usuario con la id  del curso
+      this.db.collection('asesorias').doc(user.uid + '@' + this.data.curso.id).set({
 
-     dias:this.asesorias
-   }).finally(()=>{
-     console.log("Guardado exitoso");
-     
-   }).catch((err)=>{
-     console.log(err);
-     
-   })
-  
-}else{
-  //Aqui se puede poner una alerta 
-  console.log("No se a selecionado todas las clases")
- 
-}
-    
+        dias: this.asesorias
+      }).finally(() => {
+        this.msg.msgSuccess('Exito', 'Se  registro correctamente')
+        console.log("Guardado exitoso");
+
+        this.router.navigate(['/']);
+        this.dialogRef.close();
+
+
+      }).catch((err) => {
+        console.log(err);
+
+      })
+
+    } else {
+      //Aqui se puede poner una alerta 
+      console.log("No se a selecionado todas las clases")
+
+    }
+
   }
 
-  
-  pay(){
-  
-  }
 }
