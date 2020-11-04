d15aca3638d6a7e7fe91a5a1994d9bab288dfaa7
Sebas <sebastianrnoriega.hotmail.com>
2020-11-03 19:30:18 -0700

    Dialogo nueva certificacion

--- src/app/certificacionesinstructor/certificacionesinstructor.component.ts
+++ src/app/certificacionesinstructor/certificacionesinstructor.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { MsgService } from 'src/services/msg.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from 'src/models/categoria';
+import { Usuario } from 'src/models/usuario';
 
 
 @Component({
@@ -9,18 +16,20 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 })
 export class CertificacionesinstructorComponent implements OnInit {
 
+
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+
   }
+
   openDialog(): void {
 
     const dialogRef = this.dialog.open(dialogNewcertificacion, {
       width: '800px',
-      height: '300px',
+      height: '400px',
     });
   }
-
 }
 @Component({
   selector: 'dialogNewcertificacion',
@@ -28,8 +37,42 @@ export class CertificacionesinstructorComponent implements OnInit {
   styleUrls: ['./dialogNewcertificacion.css'],
 })
 export class dialogNewcertificacion implements OnInit {
-  constructor() { }
+  categoria: Categoria
+  categorias: Array<Categoria> = new Array()
+  categoriasUsadas: Array<Categoria> = new Array()
+  usuario: Usuario
+  constructor(public dialogRef: MatDialogRef<dialogNewcertificacion>,
+    @Inject(MAT_DIALOG_DATA) public data: any, private db: AngularFirestore, private msg: MsgService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    categoria: ['', Validators.required]
+    this.getMyCategories()
+    this.getCategories()
+  }
+  getCategories() {
+    this.db.collection('categorias').get().subscribe((res) => {
+      res.docs.forEach((item) => {
+        let c = item.data() as Categoria
+        c.id = item.id;
+        this.categorias.push(c)
+        for (let iten of this.categoriasUsadas) {
+          if (c.nombre == iten.nombre) {
+            this.categorias.pop();
+          }
+        }
+      })
+
+    })
+  }
+  getMyCategories() {
+    this.db.collection('cursos').get().subscribe((res) => {
+      res.docs.forEach((item) => {
+        let h = item.data();
+        if (h.user.uid == this.usuario.uid) {
+          this.categoriasUsadas.push(h.categoria);
+          console.log("cat usada " + h.categoria.nombre);
+        }
+      })
+    })
   }
 }
