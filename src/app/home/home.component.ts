2795d6025737e0728e7d9aa7179616931e17e819
Jospam <pauljmm99@gmail.com>
2020-11-22 01:28:04 -0700

    Funciona carrusel faltan ajustes y los solicitados

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,19 +23,7 @@ export class HomeComponent implements OnInit {
   singleSlideOffset = true;
   noWrap = true;
 
-  slides = [
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/3.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/4.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/5.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/6.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/7.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/8.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/1.jpg' },
-    { image: 'https://valor-software.com/ngx-bootstrap/assets/images/nature/2.jpg' }
-  ];
-
+  
   constructor(private db: AngularFirestore, private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
