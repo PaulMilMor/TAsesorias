84d8a6a163a88454d08b50047eac0694d9f0594e
Jospam <pauljmm99@gmail.com>
2020-11-29 02:38:40 -0700

    Dialogo de suspensión de instructores

--- src/app/adminreport/adminreport.component.ts
+++ src/app/adminreport/adminreport.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Inject} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MsgService } from 'src/services/msg.service';
@@ -6,7 +6,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Reporte } from 'src/models/reportes';
 import { Usuario } from 'src/models/usuario';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import {Sort} from '@angular/material/sort';
+import { Sort } from '@angular/material/sort';
 @Component({
   selector: 'app-adminreport',
   templateUrl: './adminreport.component.html',
@@ -17,18 +17,18 @@ export class AdminreportComponent implements OnInit {
   sortedData: any[];
   constructor(private db: AngularFirestore, private msg: MsgService, public dialog: MatDialog) {
     this.sortedData = this.reportes.slice();
-   }
+  }
 
   ngOnInit(): void {
-this.getReportes()
+    this.getReportes()
 
   }
   openDialog(maestro): void {
     const dialogRef = this.dialog.open(dialogBan, {
-   
- data:{
-    maestro:maestro
- }
+      width: '510px', height: '320px',
+      data: {
+        maestro: maestro
+      }
     });
   }
   getReportes() {
@@ -37,12 +37,12 @@ this.getReportes()
         let r = item.data() as Reporte
         r.id = item.id
         console.log(item.data().fecha);
-      
-        r.fecha=new Date((item.data().fecha.seconds)*1000)
- 
+
+        r.fecha = new Date((item.data().fecha.seconds) * 1000)
+
         this.reportes.push(r)
-        
-        
+
+
       })
     })
     this.sortedData = this.reportes
@@ -51,32 +51,32 @@ this.getReportes()
 
   sortData(sort: Sort) {
     console.log(sort);
-    
+
     const data = this.reportes.slice();
     if (!sort.active || sort.direction === '') {
       this.sortedData = data;
       return;
     }
-  
+
     this.sortedData = data.sort((a, b) => {
       const isAsc = sort.direction === 'asc';
       switch (sort.active) {
         case 'instructor': return compare(a.maestro.nombre, b.maestro.nombre, isAsc);
         case 'fecha': return compare(a.fecha, b.fecha, isAsc);
         case 'reporte': return compare(a.reporte, b.reporte, isAsc);
-       
-    
+
+
         default: return 0;
       }
     });
   }
-  }
-  
-  function compare(a: number | string | Date, b: number | string |Date, isAsc: boolean) {
+}
+
+function compare(a: number | string | Date, b: number | string | Date, isAsc: boolean) {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-  }
-   
- 
+}
+
+
 
 @Component({
   selector: 'dialogBan',
@@ -93,42 +93,46 @@ export class dialogBan {
 
   }
 
-  banHammer(){
-    var fechaInicio=new Date()
-    var fechaFinal=new Date()
-    fechaFinal.setDate(fechaFinal.getUTCDate()+15)
+  banHammer() {
+    var fechaInicio = new Date()
+    var fechaFinal = new Date()
+    fechaFinal.setDate(fechaFinal.getUTCDate() + 15)
     this.db.collection('baneados').doc(this.data.maestro.uid).set({
-      fechaInicio:fechaInicio,
-      fechaFinal:fechaFinal,
-      tipoSuspension:'Manual',
-      maestro:this.data.maestro 
-    }).finally(()=>{
+      fechaInicio: fechaInicio,
+      fechaFinal: fechaFinal,
+      tipoSuspension: 'Manual',
+      maestro: this.data.maestro
+    }).finally(() => {
       this.deleteReportes(this.data.maestro.uid)
     })
   }
-  banHammerU(){
-    var fechaInicio=new Date()
-    var fechaFinal='Indefinidamente'
+  banHammerU() {
+    var fechaInicio = new Date()
+    var fechaFinal = 'Indefinidamente'
 
     this.db.collection('baneados').doc(this.data.maestro.uid).set({
-      fechaInicio:fechaInicio,
-      fechaFinal:fechaFinal,
-      tipoSuspension:'Manual',
-      maestro:this.data.maestro 
-    }).finally(()=>{
+      fechaInicio: fechaInicio,
+      fechaFinal: fechaFinal,
+      tipoSuspension: 'Manual',
+      maestro: this.data.maestro
+    }).finally(() => {
       this.deleteReportes(this.data.maestro.uid)
     })
   }
-  deleteReportes(id){
-    this.db.collection('reportes').get().subscribe(res=>{
-      res.forEach(item=>{
-        let r=item.data() as Reporte
-         r.id=item.id
-        if(r.maestro.uid==id){
+  deleteReportes(id) {
+    this.db.collection('reportes').get().subscribe(res => {
+      res.forEach(item => {
+        let r = item.data() as Reporte
+        r.id = item.id
+        if (r.maestro.uid == id) {
           this.db.collection('reportes').doc(r.id).delete()
         }
       })
       window.location.reload()
     })
+  }
+
+  cancel(){
+    this.dialogRef.close();
   }
 }
